97653ad23b42c04dede017d0902c1bbc1a2acb6f
Alex Marin Mendez <alexmarinmendez@gmail.com>
2021-09-02 11:57:56 -0500

    Add FORCE as an environment variable

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const { conn } = require('./src/db');
 const { bulkCreateCategories } = require('./src/controllers/categoriesLoader');
 const { bulkCreateProducts } = require('./src/controllers/productsLoader');
 
-// const force = (process.env.FORCE || false)
+const force = (process.env.FORCE || false);
 
-conn.sync({ force: true })
-    .then(() => bulkCreateCategories())
-    .then(() => bulkCreateProducts())
+conn.sync({ force })
+    .then(async () => force ? await bulkCreateCategories() : null)
+    .then(async () => force ? await bulkCreateProducts(): null)
     .then(() => {
         server.listen(3001, () => {
             console.log('%s listening at 3001');
